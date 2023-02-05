520849133b12e2347549551c15af2384906a9f42
Yuta NISHIMURA <wh317706@gmail.com>
2023-02-05 23:10:43 +0900

    add scheduler

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,13 @@ use cortex_m_semihosting::hprintln;
 
 mod systick;
 mod process;
-use process::ContextFrame;
+use process::{AlignedStack, Process};
 
 mod linked_list;
+use linked_list::ListItem;
 
-#[repr(align(8))]
-struct AlignedStack(MaybeUninit<[u8; 1024]>);
+mod scheduler;
+use scheduler::Scheduler;
 
 const CFSR_ADDR: usize = 0xE000_ED28;
 const SHCSR_ADDR: usize = 0xE000_ED24;
@@ -42,25 +43,24 @@ pub unsafe extern "C" fn Reset() -> ! {
 
     #[link_section = ".app_stack"]
     static mut APP_STACK: AlignedStack = AlignedStack(MaybeUninit::uninit());
-    hprintln!("APP_STACK ptr:{:p}", &APP_STACK).unwrap();
-    let ptr = (APP_STACK.0.as_ptr() as *const u8 as usize) + 1024 - 0x20;
-    let context_frame: &mut ContextFrame = &mut *(ptr as *mut ContextFrame);
-    hprintln!("context_frame ptr:{:p}", context_frame).unwrap();
-    context_frame.r0 = 0;
-    context_frame.r1 = 0;
-    context_frame.r2 = 0;
-    context_frame.r3 = 0;
-    context_frame.r12 = 0;
-    context_frame.lr = 0;
-    context_frame.return_addr = app_main as u32;
-    context_frame.xpsr = 0x0100_0000;
+    #[link_section = ".app_stack"]
+    static mut APP_STACK2: AlignedStack = AlignedStack(MaybeUninit::uninit());
+    #[link_section = ".app_stack"]
+    static mut APP_STACK3: AlignedStack = AlignedStack(MaybeUninit::uninit());
 
-    hprintln!("CFSR:{:X}", ptr::read_volatile(CFSR_ADDR as *mut u32)).unwrap();
-    hprintln!("SHCSR:{:X}", ptr::read_volatile(SHCSR_ADDR as *mut u32)).unwrap();
-    hprintln!("HFSR:{:X}", ptr::read_volatile(HFSR_ADDR as *mut u32)).unwrap();
-    asm_execute_process(ptr);
+    let process = Process::new(&mut APP_STACK, app_main);
+    let mut item = ListItem::new(process);
+    let process2 = Process::new(&mut APP_STACK2, app_main2);
+    let mut item2 = ListItem::new(process2);
+    let process3 = Process::new(&mut APP_STACK3, app_main3);
+    let mut item3 = ListItem::new(process3);
+    let mut scheduler = Scheduler::new();
 
-    loop {}
+    scheduler.push(&mut item);
+    scheduler.push(&mut item2);
+    scheduler.push(&mut item3);
+
+    scheduler.exec();
 }
 
 #[link_section = ".vector_table.reset_vector"]
@@ -167,12 +167,26 @@ pub extern "C" fn PendSV() {
 pub extern "C" fn SysTick() {
     hprintln!("Sysick").unwrap();
 }
+
 extern "C" fn app_main() -> ! {
-    hprintln!("App").unwrap();
-    unsafe { asm!("svc 0"); }
-    loop {}
+    let mut i = 0;
+    loop {
+        hprintln!("App {}", i).unwrap();
+        unsafe { asm!("svc 0"); }
+        i += 1;
+    }
 }
 
-extern "C" {
-    fn asm_execute_process(sp: usize);
+extern "C" fn app_main2() -> ! {
+    loop {
+        hprintln!("App2").unwrap();
+        unsafe { asm!("svc 0"); }
+    }
+}
+
+extern "C" fn app_main3() -> ! {
+    loop {
+        hprintln!("App3").unwrap();
+        unsafe { asm!("svc 0"); }
+    }
 }
--- src/process.rs
+++ src/process.rs
@@ -1,3 +1,6 @@
+use core::marker::PhantomData;
+use core::mem::MaybeUninit;
+
 #[repr(C)]
 pub struct ContextFrame {
     pub r0: u32,
@@ -8,4 +11,44 @@ pub struct ContextFrame {
     pub lr: u32,
     pub return_addr: u32,
     pub xpsr: u32,
+}
+
+pub struct Process<'a> {
+    sp: usize,
+    regs: [u32; 8],
+    marker: PhantomData<&'a u8>,
+}
+
+#[repr(align(8))]
+pub struct AlignedStack(pub MaybeUninit<[u8; 1024]>);
+
+extern "C" {
+    fn asm_execute_process(sp: usize, regs: &mut [u32; 8]) -> usize;
+}
+
+impl<'a> Process<'a> {
+    pub fn new(stack: &'a mut AlignedStack, app_main: extern "C" fn() -> !) -> Self {
+        let sp = (stack.0.as_ptr() as usize) + unsafe { stack.0.assume_init_ref().len() } - 0x20;
+        let context_frame: &mut ContextFrame = unsafe {
+            &mut *(sp as *mut ContextFrame)
+        };
+        context_frame.r0 = 0;
+        context_frame.r1 = 0;
+        context_frame.r2 = 0;
+        context_frame.r3 = 0;
+        context_frame.r12 = 0;
+        context_frame.lr = 0;
+        context_frame.return_addr = app_main as u32;
+        context_frame.xpsr = 0x0100_0000;
+
+        Process {
+            sp,
+            regs: [0; 8],
+            marker: PhantomData,
+        }
+    }
+
+    pub fn exec(&mut self) {
+        self.sp = unsafe { asm_execute_process(self.sp, &mut self.regs) }
+    }
 }
